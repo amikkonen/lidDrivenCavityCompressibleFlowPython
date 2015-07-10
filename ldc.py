2ce7766174eb92e7429870a55a334f1d0cc335b7
Antti Mikkonen <a.mikkonen@iki.fi>
2015-07-10 08:19:07 +0300

    typos

--- ldc.py
+++ ldc.py
@@ -11,8 +11,8 @@ given at AALTO university, Finland, in 2015. Being a course exercise
 this code was never rigorously tested but according to the test data by
 Ghia et al. (1982) (function ghia in this code) it works fine. I'm just 
 publicing this out of a habit. Someone might find it useful while learning 
-Python. A short given on the subject containing some of the equations should be 
-included.
+Python. A short presentation given on the subject containing some of the 
+equations should be included.
 
 Lisence: DO WHATEVER YOU WANT. A Citation would be appreciated.
 
